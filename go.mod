ffd9db403f5a9fe674b14c805dcbd0e554a0f202
christopher.dziomba@telekom.de <christopher.dziomba@telekom.de>
2021-12-26 15:00:28 +0100

    Configuration and support for multiple nanoleafs

--- go.mod
+++ go.mod
@@ -5,15 +5,32 @@ go 1.17
 require (
 	github.com/adnanbrq/nanoleaf v0.0.0-20201213184944-913d5ef05e03
 	github.com/jsimonetti/go-artnet v0.0.0-20210922080205-810e8e5e57a2
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/viper v1.10.1
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-resty/resty/v2 v2.2.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/mdns v1.0.4 // indirect
+	github.com/libp2p/go-reuseport v0.1.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/miekg/dns v1.1.41 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
-replace github.com/jsimonetti/go-artnet => github.com/chdxD1/go-artnet v0.0.0-20211220220028-30ac920ae1d9
+replace github.com/jsimonetti/go-artnet => github.com/chdxD1/go-artnet v0.0.0-20211226110725-9139deaeae59
 
-replace github.com/adnanbrq/nanoleaf => github.com/chdxD1/nanoleaf-go v0.0.0-20211220204931-24fad2cbe599
+replace github.com/adnanbrq/nanoleaf => github.com/chdxD1/nanoleaf-go v0.0.0-20211226111846-f6995b471c56
--- main.go
+++ main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"net"
-	"os"
+	"fmt"
 	"sort"
 	"time"
 
@@ -10,52 +9,215 @@ import (
 	"github.com/jsimonetti/go-artnet"
 	"github.com/jsimonetti/go-artnet/packet"
 	"github.com/jsimonetti/go-artnet/packet/code"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	nlfs := nanoleaf.NewNanoleaf(os.Getenv("NANOLEAF_URL"))
-	nlfs.SetToken(os.Getenv("NANOLEAF_TOKEN"))
+type nanoleafConfig struct {
+	Api   string `mapstructure:"api"`
+	Token string `mapstructure:"token"`
+}
 
-	err := nlfs.Stream.Activate(nanoleaf.VersionV2)
-	if err != nil {
-		panic(err)
-	}
+type artnetConfig struct {
+	InterfaceName string `mapstructure:"interfaceName"`
+	StartAddress  uint16 `mapstructure:"startAddress"`
+}
 
-	layout, _ := nlfs.Layout.GetLayout()
-	positions := layout.PositionData
-	sort.Slice(positions, func(i, j int) bool {
-		if positions[i].Y == positions[j].Y {
-			return positions[i].X < positions[j].X
-		} else {
-			return positions[i].Y > positions[j].Y
+type config struct {
+	Nanoleafs []nanoleafConfig `mapstructure:"nanoleafs"`
+	ArtNet    artnetConfig     `mapstructure:"artnet"`
+}
+
+type ArtNetNanoleaf struct {
+	nanoleaf             *nanoleaf.Nanoleaf
+	startUniverseAddress uint16
+	consumedUniverses    uint16
+	positionData         []nanoleaf.PanelPositionData
+}
+
+var (
+	DMX_CHANNELS      = 512
+	CHANNEL_PER_PANEL = 4
+	PANEL_IN_UNIVERSE = DMX_CHANNELS / CHANNEL_PER_PANEL
+)
+
+func waitForAllTokens(nlfs []*nanoleaf.Nanoleaf) {
+	// First identify/flash all Nanoleafs where we have a token, remove token if Unauthorized
+	for _, nlf := range nlfs {
+		if nlf.Identity.Flash() == nanoleaf.ErrUnauthorized {
+			nlf.SetToken("")
 		}
-	})
+	}
 
-	err = nlfs.Stream.Connect()
+	for {
+		waitingAuth := 0
+		for _, nlf := range nlfs {
+			if nlf.IsConnected() {
+				continue
+			}
+			fmt.Printf("Trying to authenticate nanoleaf at %s\n", nlf.GetUrl())
+			if nlf.Auth.Authenticate() != nil {
+				fmt.Printf("nanoleaf at %s not yet ready - please set to pairing mode according to guide\n", nlf.GetUrl())
+				waitingAuth++
+			} else {
+				fmt.Printf("nanoleaf at %s successfully authenticated", nlf.GetUrl())
+			}
+
+		}
+		if waitingAuth == 0 {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func main() {
+	log := artnet.NewLogger(logrus.New().WithFields(nil))
+
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("fatal error config file: %w", err)
+	}
+
+	var C config
+
+	viper.Unmarshal(&C)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+
+	// If no Nanoleafs were configured we will first discover them through mDNS
+	if len(C.Nanoleafs) == 0 {
+		nanoleafs, err := nanoleaf.DiscoverNanoleafs(5 * time.Second)
+		if err != nil {
+			log.Fatalf("couldn't discover Nanoleafs, %v", err)
+		}
+		for _, discoveredNanoleafs := range nanoleafs {
+			C.Nanoleafs = append(C.Nanoleafs, nanoleafConfig{
+				Api: discoveredNanoleafs.GetUrl(),
+			})
+		}
+		viper.Set("nanoleafs", C.Nanoleafs)
+		viper.WriteConfig()
+	}
+
+	nanoleafs := []*nanoleaf.Nanoleaf{}
+	for _, nanoleafAPI := range C.Nanoleafs {
+		nlf := nanoleaf.NewNanoleaf(nanoleafAPI.Api)
+		if len(nanoleafAPI.Token) > 0 {
+			nlf.SetToken(nanoleafAPI.Token)
+		}
+		nanoleafs = append(nanoleafs, nlf)
+	}
+
+	// If some Nanoleafs do not have a token set, try to login for some time
+	waitForAllTokens(nanoleafs)
+
+	artNetNanoleafs := []ArtNetNanoleaf{}
+	universeOffset := uint16(0)
+
+	for _, nlfs := range nanoleafs {
+		err = nlfs.Stream.Activate(nanoleaf.VersionV2) // TODO: Autodetect version 1 / version 2
+		if err != nil {
+			panic(err)
+		}
+
+		layout, _ := nlfs.Layout.GetLayout()
+		positions := layout.PositionData
+		sort.Slice(positions, func(i, j int) bool {
+			if positions[i].Y == positions[j].Y {
+				return positions[i].X < positions[j].X
+			} else {
+				return positions[i].Y > positions[j].Y
+			}
+		})
+
+		err = nlfs.Stream.Connect()
+		if err != nil {
+			panic(err)
+		}
+
+		neededDMXAddresses := len(positions) * CHANNEL_PER_PANEL
+		neededUniverses := uint16((neededDMXAddresses + DMX_CHANNELS - 1) / DMX_CHANNELS)
+
+		artNetNanoleafs = append(artNetNanoleafs, ArtNetNanoleaf{
+			nanoleaf:             nlfs,
+			startUniverseAddress: C.ArtNet.StartAddress + universeOffset,
+			consumedUniverses:    neededUniverses,
+			positionData:         positions,
+		})
+		universeOffset += neededUniverses
+	}
+
+	// Initialize Art-Net node
+	node, err := artnet.NewNode("nanoleaf", code.StNode, C.ArtNet.InterfaceName, log)
 	if err != nil {
 		panic(err)
 	}
 
-	log := artnet.NewDefaultLogger()
-	node := artnet.NewNode("nanoleaf", code.StNode, net.ParseIP(os.Getenv("ARTNET_NODE_IP")), log)
+	// Add ports for universe
+	for universe := uint16(0); universe < universeOffset; universe++ {
+		node.Config.OutputPorts = append(node.Config.OutputPorts, artnet.OutputPort{
+			Address: artnet.AddressFromInt(universe),
+			Type:    code.PortType(0).WithType("DMX512"),
+		})
+	}
 
+	// Calback for DMX data packet
 	node.RegisterCallback(code.OpDMX, func(p packet.ArtNetPacket) {
 		dmxPacket := p.(*packet.ArtDMXPacket)
 
+		artnetAddress := artnet.Address{
+			Net:    dmxPacket.Net,
+			SubUni: dmxPacket.SubUni,
+		}
+		address := uint16(artnetAddress.Integer())
+
+		// Find initialized nanoleaf by universe
+		var artNetNanoleaf *ArtNetNanoleaf
+		offset := 0
+		for _, annlf := range artNetNanoleafs {
+			endUniverse := annlf.startUniverseAddress + annlf.consumedUniverses - 1
+			if annlf.startUniverseAddress <= address && address <= endUniverse {
+				artNetNanoleaf = &annlf
+				offset = int(address - annlf.startUniverseAddress)
+				break
+			}
+		}
+		if artNetNanoleaf == nil {
+			fmt.Printf("Did not find corresponding Nanoleaf for received ArtDMXPacket in universe %s\n", artnetAddress.String())
+			return
+		}
+
 		panels := []nanoleaf.PanelEffect{}
 
-		for i, panel := range positions {
+		// Calculate panels to update
+		positionStart := offset * PANEL_IN_UNIVERSE
+		positionEnd := min((offset+1)*PANEL_IN_UNIVERSE, len(artNetNanoleaf.positionData))
+
+		for i, panel := range artNetNanoleaf.positionData[positionStart:positionEnd] {
 			panels = append(panels, nanoleaf.PanelEffect{
 				ID: panel.ID,
 				Frame: nanoleaf.FrameEffect{
-					Red:   int(dmxPacket.Data[i*3]),
-					Green: int(dmxPacket.Data[i*3+1]),
-					Blue:  int(dmxPacket.Data[i*3+2]),
+					Red:   int(dmxPacket.Data[i*CHANNEL_PER_PANEL+0]),
+					Green: int(dmxPacket.Data[i*CHANNEL_PER_PANEL+1]),
+					Blue:  int(dmxPacket.Data[i*CHANNEL_PER_PANEL+2]),
+					White: int(dmxPacket.Data[i*CHANNEL_PER_PANEL+3]),
 				},
 			})
 		}
 
-		nlfs.Stream.WriteEffect(nanoleaf.StreamEffect{
+		artNetNanoleaf.nanoleaf.Stream.WriteEffect(nanoleaf.StreamEffect{
 			Panels: panels,
 		})
 	})
